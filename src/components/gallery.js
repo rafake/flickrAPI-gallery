475a0f24175da95938aa1b3fdebd7ed84c7d4976
Rafal Kelm <rafal.kelm@gmail.com>
2019-08-11 00:22:09 +0200

    input field

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -15,7 +15,14 @@ class Gallery extends React.Component{
             author: "",
             description: "",
             isOpen: false,
-        }
+            pageNo: 2,
+            value: "",
+            tag: this.props.type,
+            hasUpdated: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     openImage = (imgInfo) => {
@@ -38,11 +45,40 @@ class Gallery extends React.Component{
     };
 
     componentDidMount() {
-        const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=67fa847a53bdc09b2db5c74c43d44b39&tags=${this.props.type}&sort=interestingness-desc&content_type=1&per_page=20&format=json&nojsoncallback=1`
+        const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=67fa847a53bdc09b2db5c74c43d44b39&tags=${this.state.tag}&sort=interestingness-desc&content_type=1&per_page=20&format=json&nojsoncallback=1`
         fetch(url).then(r=>r.json()).then(resp=> this.setState({photos: resp.photos.photo}));
         this.hoverEffectFunction();
     }
+    //
+    // componentWillUpdate(nextProps, nextState, nextContext) {
+    // }
 
+    componentWillUpdate(nextProps, nextState, nextContext) {
+            console.log(nextState)
+            const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=67fa847a53bdc09b2db5c74c43d44b39&tags=${this.state.tag}&sort=interestingness-desc&content_type=1&per_page=20&format=json&nojsoncallback=1`
+            fetch(url).then(r=>r.json()).then(resp=> this.setState({photos: resp.photos.photo}));
+            this.hoverEffectFunction();
+
+    }
+
+// handling loading more photos
+    handleLoadMore = () => {
+        // counter specifing which page of the results will be displayed
+        let counter = this.state.pageNo;
+        counter++;
+        this.setState({pageNo: counter});
+        console.log(this.state.pageNo);
+        const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=67fa847a53bdc09b2db5c74c43d44b39&tags=${this.props.type}&sort=interestingness-desc&content_type=1&per_page=20&page=${this.state.pageNo}&format=json&nojsoncallback=1`
+        fetch(url).then(r=>r.json()).then(resp=> {
+            console.log(resp);
+            let photosOnLoad = resp.photos.photo;
+            let photosLoadedPreviously = this.state.photos;
+            let newPhotosState = photosLoadedPreviously.concat(photosOnLoad);
+            console.log(newPhotosState);
+            this.setState({photos: newPhotosState})}
+        );
+    };
+// main Hero hover effect
     hoverEffectFunction = () => {
         let myAnimation = new hoverEffect({
             parent: document.querySelector('.my-div'),
@@ -57,16 +93,29 @@ class Gallery extends React.Component{
             // next: 1
             speedIn: 5
         });
-    //     return (
-    //         <>
-    //     <div className="my-div"></div>
-    //         </>
-    // )
     };
 
+// form handlers
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({tag: this.state.value});
+        this.setState({hasUpdated: true});
+        setTimeout(() => {
+            console.log(this.state.tag);
+        },1000);
+        this.setState({hasUpdated: false});
+
+
+    }
+
+// render function
     render() {
         const images = this.state.photos.map( (el,i) => {
-            console.log(this.state.photos);
             return <Picture key={i}
                             farm={el.farm}
                             server={el.server}
@@ -75,37 +124,58 @@ class Gallery extends React.Component{
                             openImage={this.openImage}/>
         });
         return (<>
-            {/*<div className="hoverEffect__outerWrapper">*/}
-            {/*    <div className="hoverEffect__innerWrapper">*/}
-            {/*        {this.hoverEffectFunction()}*/}
-            {/*    </div>*/}
-            {/*</div>*/}
 
             <header>
-                <div className="my-div"></div>
-                <h1>
-                fetching photos from flickr
+                <div className="header__menuWrapper">
+                    <div className="header__innerWrapper">
+                        <h1>
+                            fetching photos from <a href="https://www.flickr.com/services/api/" target="_blank" className="header__link header__link--flickr"><i>flickr API</i></a>
+                            <img src={flickrIcon} className="fi-flickr"/>
+                        </h1>
 
-                </h1>
-                <img src={flickrIcon} className="fi-flickr"/>
-
+                        <a href="https://rafake.github.io/" target="_blank" className="header__link">visit <i>rafake.github.io</i></a>
+                    </div>
+                </div>
             </header>
+            <div className="hero__wrapper">
+                <div className="hero__insideWrapper">
+                    <div className="my-div"></div>
+
+                </div>
+
+            </div>
             <main className="gallery__outerWrapper">
+
             <section className="gallery__innerWrapper">
-                {/*<Masonry className={'gallery'}*/}
-                {/*         elementType={'ul'}*/}
-                {/*>*/}
+
+                {/*input form section - submitting tags*/}
+                <form onSubmit={this.handleSubmit} className="gallery__form">
+                    <label className="gallery__label">
+                        Enter the keyword
+                        <input type="text" value={this.state.value} onChange={this.handleChange} className="gallery__inputField"/>
+                    </label>
+                    <input type="submit" value="Enter" className="gallery__inputButton"/>
+                </form>
+
+                {/*gallery section - displaying photos*/}
+
                 <div className="gallery">
+
                 <ul>
                 {images}
                 {/*</Masonry>*/}
                 </ul>
                 </div>
                 {
-                    this.state.isOpen && <ModalPicture author={this.state.author} path={this.state.path} description={this.state.description} onClickClose={this.onClickClose}/>
+                    this.state.isOpen && <ModalPicture author={this.state.author} path={this.state.path} description={this.state.description} onClickClose={this.onClickClose} className="modalInvisible"/>
                 }
             </section>
+
             </main>
+                <footer>
+                    <button className="footer__loadMore" onClick={this.handleLoadMore}>Load more</button>
+                </footer>
+
             </>
         );
     }
