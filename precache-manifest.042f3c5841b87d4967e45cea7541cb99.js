cbcab05cea7fafa0467e62c39c8a7ab83e3cf031
Rafal Kelm <rafal.kelm@gmail.com>
2019-08-11 19:24:49 +0200

    Updates

---
+++ precache-manifest.042f3c5841b87d4967e45cea7541cb99.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6ac2deb7eb852b318ea001c0d37f1d",
+    "url": "/flickrAPI-gallery/index.html"
+  },
+  {
+    "revision": "2b82b58c908c2ba659ef",
+    "url": "/flickrAPI-gallery/static/css/main.80df3f33.chunk.css"
+  },
+  {
+    "revision": "37194989d1dab89bbedf",
+    "url": "/flickrAPI-gallery/static/js/2.8258aebf.chunk.js"
+  },
+  {
+    "revision": "2b82b58c908c2ba659ef",
+    "url": "/flickrAPI-gallery/static/js/main.d318ec84.chunk.js"
+  },
+  {
+    "revision": "dee2472512a4936d4657",
+    "url": "/flickrAPI-gallery/static/js/runtime~main.670a2a96.js"
+  },
+  {
+    "revision": "3fe559ed4f313fdb528634577ede5cee",
+    "url": "/flickrAPI-gallery/static/media/adventure-daylight-glacier-301391.3fe559ed.jpg"
+  },
+  {
+    "revision": "a0e647873c17e3d16dfddd3bd48ca1b7",
+    "url": "/flickrAPI-gallery/static/media/blue-sky-clear-sky-cold-346529.a0e64787.jpg"
+  },
+  {
+    "revision": "6aef47bea2ab928fb48a45725ff7d96b",
+    "url": "/flickrAPI-gallery/static/media/daylight-environment-forest-459225.6aef47be.jpg"
+  },
+  {
+    "revision": "f03cabeded13597dd813a4ff9d2ccc23",
+    "url": "/flickrAPI-gallery/static/media/flickr.f03cabed.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/flickrAPI-gallery/static/media/logo.ee7cd8ed.svg"
+  }
+]);
